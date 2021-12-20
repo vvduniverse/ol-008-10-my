2eab0a5b971c1a318eb36e86dcf88faddaf4f2e1
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-20 16:20:50 +0200

    Commit

--- ol-008-10-my/ContentView.swift
+++ ol-008-10-my/ContentView.swift
@@ -7,10 +7,29 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    private let headers = ["Title One", "Title Two", "Title Three", "Title Four", "Title Five"]
+    @State private var index = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+            Image(systemName: "plus.rectangle.on.folder")
+                .resizable()
+                .frame(width: 300, height: 200)
+                Spacer()
+            Button("Change the title") {
+                if index < headers.count - 1 {
+                index += 1
+                    print(index)
+                }
+            }
+            }
+            .navigationTitle(headers[index])
+        }
     }
 }
 
